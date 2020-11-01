9d75cd76a3db155cc90076746c31a4ddaaf97852
Geno Ferollo <genemators@gmail.com>
2020-11-01 08:04:04 +0500

    uploading all tests

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def main():
+    pass
+
+
+def test_function(x):
+    return x + 1
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_main.py
@@ -0,0 +1,9 @@
+from main import test_function
+
+
+def test():
+    assert test_function(3) == 4
+
+
+if __name__ == '__main__':
+    test()
