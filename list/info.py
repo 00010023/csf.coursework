989284bd34ee6109e39c888bdc6f5a77dc36934e
Geno Ferollo <genemators@gmail.com>
2020-12-04 11:37:08 +0500

    into

--- list/info.py
+++ list/info.py
@@ -9,25 +9,25 @@ Copyright to 000010023 by WIUT student
 example_list = ["green", 123456789, True, [], {}, ()]
 
 information1 = \
-    f"""
+    """
 Lists on Python
 
 List is a type of Python Collection where it's ordered
 and any data containing python list can be changed or replaced!
-Lists are appointed with curly brackets and it can contain strings,
+Lists are appointed with square brackets and it can contain strings,
 numbers, another lists, tuples & dictionaries...
 
 Let me show an example:
-I'd like to assign "example = ["green", 123456789, True, [], ()]"
+I'd like to assign "example_list = ["green", 123456789, True, [], {}, ()]"
 list and let's see result...
 
 OUTPUT
 ============
-{example_list}
 """
 
 
 def info():
     print(information1)
+    print(example_list)
     input("Continue? [Press Enter]")
     pass
