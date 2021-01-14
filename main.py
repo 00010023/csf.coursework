42db791e3a7520eb145812354ede62c438fca6f6
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-01-14 10:58:50 +0500

    forgot exclude stage

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ created to fulfill WIUT's requirements
 Copyright to 000010023 by WIUT student
 """
 
-from stage import launcher
-
-
 if __name__ == '__main__':
+    print("Hello World")
     pass
     # while True:
     #     launcher()
