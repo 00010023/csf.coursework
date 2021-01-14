988f12ec655a04d94c6e98d50250fb77908f1ab1
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-01-14 10:32:51 +0500

    cleaning up main branch

--- start-win.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo "======= INSTALLATION ======="
-pip install -r requirements.txt
-echo "======= RUNNING APP ======="
-python3 main.py
-pause
--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Copyright to 000010023 by WIUT student
-"""
-
-from .test_colorly import *
-from .test_stage import *
